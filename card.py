27cdd85d5d4adae98bb296db3cbce8af53bf7104
ImplyingICheck <amhector@stanford.edu>
2023-05-09 15:33:54 -0700

    feat: Add card class
    Add card class. Implement __init__ and static function
    __set_field_names.
    
    The init is based on documentation from the Anki
    manual. All class properties follow the naming scheme set by Anki.
    Exceptions: columns from Anki is renamed to field_names.
    
    Write a static
    method to generate generic names for class fields in case the
    field_names is not passed in.

---
+++ card.py
@@ -0,0 +1,30 @@
+"""Base class for Anki card"""
+
+class Card:
+  """
+  Anki Card fields as denoted by Anki documentation
+  Up to date reference:
+  https://docs.ankiweb.net/importing.html#file-headers
+  Permanent Reference [09 May 2023]:
+  https://github.com/ankitects/anki-manual/blob/0aa372146d10e299631e361769f41533a6d4a417/src/importing.md?plain=1#L196-L220
+  """
+  def __init__(self, fields, has_html=False, tags=None, field_names=None,
+               note_type=None, deck=None, guid=None):
+    self.has_html = has_html
+    self.tags = tags
+    self.field_names = self.__set_field_names(field_names, len(fields))
+    self.fields = fields
+    self.note_type = note_type
+    self.deck = deck
+    self.guid = guid
+
+  @staticmethod
+  def __set_field_names(field_names, n_fields):
+    if field_names:
+      return field_names
+    field_names = []
+    for idx in range(n_fields):
+      curr_field_name = f'Field{idx}'
+      field_names.append(curr_field_name)
+    return field_names
+
