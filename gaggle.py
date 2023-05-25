6e66834521bad6c4873aa06da3a3e99a2ae68ee4
ImplyingICheck <amhector@stanford.edu>
2023-05-25 15:11:20 -0700

    fix: Allow write_all_decks_to_file to have None passed in
    Helper function currently filters out all falsy values. However,
    write_deck_to_file() parameter filename has a valid value of None.

--- gaggle.py
+++ gaggle.py
@@ -325,7 +325,8 @@ class Gaggle:
       DecksNotWrittenException: If method fails to write all Decks to file.
 
     """
-    flat_kwargs = generate_flattened_kwargs_remove_falsy(**kwargs)
+    flat_kwargs = generate_flattened_kwargs_remove_sentinel(sentinel='',
+                                                            **kwargs)
     last_written_deck_idx = None
     for idx, deck in enumerate(self._get_decks()):
       self.write_deck_to_file(deck, **next(flat_kwargs, {}))
