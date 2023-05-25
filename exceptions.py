403bc78f2cc3037ee499f02a9319dfb24697f25a
ImplyingICheck <amhector@stanford.edu>
2023-05-25 16:22:28 -0700

    docs: Add documentation to DecksNotWrittenException
    Add typing hints.
    
    Add docstring to class DecksNotWrittenException.
    
    Add
    docstring to __init__ method.

--- exceptions.py
+++ exceptions.py
@@ -1,7 +1,14 @@
 """Definition of project-wide exceptions"""
 
 class DecksNotWrittenException(Exception):
+  """Gaggle exception for failure to write all stored Decks to file. Can return
+  the last successfully written deck"""
   def __init__(self, last_deck_written: int | None = None):
+    """
+    Args:
+      last_deck_written: An int representing the index of the last deck
+      successfully written to file. None if no success writes occurred.
+    """
     self.last_deck_written = last_deck_written
 
   def __str__(self):
