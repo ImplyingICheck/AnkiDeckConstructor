6f2c8ef9f531b7bcc249c89b211f3eac36668cce
Andrew <amhector@stanford.edu>
2023-07-16 00:02:05 -0700

    test: Add cases consisting of field_names
    Add cases covering all data covered by previous fixtures. Increase
    coverage.

--- cases/test_gaggle/cases_ankicard.py
+++ cases/test_gaggle/cases_ankicard.py
@@ -15,6 +15,7 @@
 # Gaggle. If not, see <https://www.gnu.org/licenses/>.
 # pylint: disable=missing-module-docstring
 # pylint: disable=missing-class-docstring
+# pylint: disable=redefined-outer-name
 import pytest_cases
 
 from gaggle import gaggle
@@ -22,7 +23,7 @@ from gaggle import gaggle
 _GENERIC_NUMBER_OF_FIELDS = 10
 _GENERIC_FIELD_VALUE = 'value'
 _GENERIC_FIELD_NAME = 'Field'
-ANKI_CARD_RESERVED_NAMES = ['GUID', 'Note Type', 'Deck', 'Tags']
+ANKI_CARD_RESERVED_NAMES = ('GUID', 'Note Type', 'Deck', 'Tags')
 
 
 class MinimumWellFormedAnkiCard:
@@ -72,7 +73,8 @@ class FullySpecifiedWellFormedAnkiCard(MinimumWellFormedAnkiCard):
     self.reserved_names = dict(
         zip([self.guid_idx, self.note_type_idx, self.deck_idx, self.tags_idx],
             ANKI_CARD_RESERVED_NAMES))
-    self.field_names = self._new_field_names_generic_with_reserved_names()
+    if not self.field_names:
+      self.field_names = self._new_field_names_generic_with_reserved_names()
     super().__init__()
 
   def _new_field_names_generic_with_reserved_names(self):
@@ -85,6 +87,14 @@ class FullySpecifiedWellFormedAnkiCard(MinimumWellFormedAnkiCard):
     return field_names
 
 
+class ModifiedFullySpecifiedAnkiCard(FullySpecifiedWellFormedAnkiCard):
+
+  def __init__(self, *, field_names=None):
+    if field_names:
+      self.field_names = field_names
+    super().__init__()
+
+
 @pytest_cases.case(tags=['FullySpecifiedAnkiCard', 'WellFormedAnkiCard'])
 def case_fully_specified_well_formed_anki_card():
   return FullySpecifiedWellFormedAnkiCard()
@@ -95,9 +105,158 @@ def case_minimum_well_formed_anki_card():
   return MinimumWellFormedAnkiCard()
 
 
-class ModifiedFullySpecifiedAnkiCard(FullySpecifiedWellFormedAnkiCard):
+@pytest_cases.case(tags=['ModifiedFullySpecifiedAnkiCard', 'Constructor'])
+def case_modified_fully_specified_anki_card():
+  return ModifiedFullySpecifiedAnkiCard
 
-  def __init__(self, *, field_names=None):
-    if field_names:
-      self.field_names = field_names
-    super().__init__()
+
+@pytest_cases.fixture
+def fully_specified_well_formed_values():
+  return FullySpecifiedWellFormedAnkiCard()
+
+
+@pytest_cases.case(tags=['Malformed', 'FieldNames', 'Surplus'])
+def field_names_surplus_by_one(fully_specified_well_formed_values):
+  field_names = fully_specified_well_formed_values.field_names
+  extra_field_names = ['This extends field_names by one.']
+  field_names.extend(extra_field_names)
+  return field_names
+
+
+@pytest_cases.case(tags=['Malformed', 'FieldNames', 'Surplus', 'Multiple'])
+def field_names_surplus_by_two(fully_specified_well_formed_values):
+  field_names = fully_specified_well_formed_values.field_names
+  extra_field_names = [
+      'This extends field_names by one.', 'This extends field_names by two.'
+  ]
+  field_names.extend(extra_field_names)
+  return field_names
+
+
+@pytest_cases.case(tags=['Well-Formed', 'FieldNames'])
+def field_names_empty_list():
+  field_names = []
+  return field_names
+
+
+@pytest_cases.case(tags=['Well-Formed', 'FieldNames'])
+def field_names_none():
+  field_names = None
+  return field_names
+
+
+@pytest_cases.case(tags=['Well-Formed', 'FieldNames'])
+def field_names_generic_well_formed(fully_specified_well_formed_values):
+  field_names = fully_specified_well_formed_values.field_names
+  return field_names
+
+
+@pytest_cases.case(tags=['Malformed', 'FieldNames', 'HeaderNameMismatch'])
+def field_names_header_name_mismatch(fully_specified_well_formed_values):
+  card_values = fully_specified_well_formed_values
+  guid_idx = card_values.guid_idx
+  field_names = card_values.field_names
+  field_names[guid_idx] = 'This is not a field name assigned by the header'
+  return field_names
+
+
+@pytest_cases.case(
+    tags=['Malformed', 'FieldNames', 'HeaderNameMismatch', 'Multiple'])
+def field_names_multiple_header_name_mismatch(
+    fully_specified_well_formed_values):
+  card_values = fully_specified_well_formed_values
+  guid_idx = card_values.guid_idx
+  note_type_idx = card_values.note_type_idx
+  field_names = card_values.field_names
+  field_names[guid_idx] = 'This is not a field name assigned by the header'
+  field_names[note_type_idx] = 'Also not a field name assigned by the header'
+  return field_names
+
+
+@pytest_cases.case(tags=[
+    'Malformed', 'FieldNames', 'GenericFieldNameOverwritten',
+    'DuplicateReservedFieldNames'
+])
+def field_names_duplicate_reserved_name_overwrites_generic(
+    fully_specified_well_formed_values):
+  card_values = fully_specified_well_formed_values
+  guid_idx = card_values.guid_idx
+  first_generic_field_name = card_values.deck_idx + 1
+  field_names = card_values.field_names
+  field_names[first_generic_field_name] = field_names[guid_idx]
+  return field_names
+
+
+@pytest_cases.case(tags=[
+    'Malformed', 'FieldNames', 'GenericFieldNameOverwritten',
+    'DuplicateReservedFieldNames', 'Multiple'
+])
+def field_names_multiple_duplicate_reserved_name_overwrites_generic(
+    fully_specified_well_formed_values):
+  card_values = fully_specified_well_formed_values
+  guid_idx = card_values.guid_idx
+  first_generic_field_name = card_values.deck_idx + 1
+  field_names = card_values.field_names
+  field_names[first_generic_field_name] = field_names[guid_idx]
+  field_names[first_generic_field_name + 1] = field_names[guid_idx]
+  return field_names
+
+
+@pytest_cases.case(tags=[
+    'Malformed', 'FieldNames', 'GenericFieldNameOverwritten',
+    'UsageAfterAssignment'
+])
+def field_names_usage_after_assignment_generic(
+    fully_specified_well_formed_values):
+  card_values = fully_specified_well_formed_values
+  first_generic_field_name = card_values.deck_idx + 1
+  field_names = card_values.field_names
+  field_names[first_generic_field_name +
+              1] = f'{_GENERIC_FIELD_NAME}{first_generic_field_name}'
+  return field_names
+
+
+@pytest_cases.case(tags=[
+    'Malformed', 'FieldNames', 'GenericFieldNameOverwritten',
+    'UsageAfterAssignment', 'Multiple'
+])
+def field_names_usage_after_assignment_generic_multiple(
+    fully_specified_well_formed_values):
+  card_values = fully_specified_well_formed_values
+  first_generic_field_name = card_values.deck_idx + 1
+  field_names = card_values.field_names
+  field_names[first_generic_field_name +
+              1] = f'{_GENERIC_FIELD_NAME}{first_generic_field_name}'
+  field_names[first_generic_field_name +
+              2] = f'{_GENERIC_FIELD_NAME}{first_generic_field_name}'
+  return field_names
+
+
+@pytest_cases.case(tags=[
+    'Malformed', 'FieldNames', 'GenericFieldNameOverwritten',
+    'UsageBeforeAssignment'
+])
+def field_names_usage_before_assignment_generic(
+    fully_specified_well_formed_values):
+  card_values = fully_specified_well_formed_values
+  first_generic_field_name = card_values.deck_idx + 1
+  field_names = card_values.field_names
+  field_names[first_generic_field_name] = (
+      f'{_GENERIC_FIELD_NAME}{first_generic_field_name +  1}')
+  return field_names
+
+
+@pytest_cases.case(tags=[
+    'Malformed', 'FieldNames', 'GenericFieldNameOverwritten',
+    'UsageBeforeAssignment', 'Multiple'
+])
+def field_names_usage_before_assignment_generic_multiple(
+    fully_specified_well_formed_values):
+  card_values = fully_specified_well_formed_values
+  first_generic_field_name = card_values.deck_idx + 1
+  field_names = card_values.field_names
+  field_names[first_generic_field_name] = (
+      f'{_GENERIC_FIELD_NAME}{first_generic_field_name + 2}')
+  field_names[first_generic_field_name +
+              1] = f'{_GENERIC_FIELD_NAME}{first_generic_field_name + 2}'
+  return field_names
